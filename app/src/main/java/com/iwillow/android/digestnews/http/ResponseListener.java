c79c6d795bd25bcdb2abdb2fdd6cd929033190d0
iwillow <iwillow@163.com>
2016-04-23 23:58:30 +0800

    add html parser to extract relative urls

--- app/src/main/java/com/iwillow/android/digestnews/http/ResponseListener.java
+++ app/src/main/java/com/iwillow/android/digestnews/http/ResponseListener.java
@@ -3,7 +3,7 @@ package com.iwillow.android.digestnews.http;
 import com.android.volley.Response;
 
 /**
- * to reduce the quantities of interfaces, create an interface which implements com.android.volley.Response.ErrorListener and com.android.volley.Response.Listener
+ * To reduce the quantities of interfaces, create an interface which implements com.android.volley.Response.ErrorListener and com.android.volley.Response.Listener
  * Created by https://www.github.com/iwillow on 2016/4/23.
  */
 public interface ResponseListener<T> extends Response.ErrorListener,
