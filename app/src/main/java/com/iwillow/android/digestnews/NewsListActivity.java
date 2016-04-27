384553e7e54fbe8fab82c2f6c2aa1389a53a1384
iwillow <iwillow@163.com>
2016-04-27 08:54:57 +0800

    New changelist

--- app/src/main/java/com/iwillow/android/digestnews/NewsListActivity.java
+++ app/src/main/java/com/iwillow/android/digestnews/NewsListActivity.java
@@ -3,18 +3,34 @@ package com.iwillow.android.digestnews;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.iwillow.android.digestnews.api.RequestAddress;
+import com.iwillow.android.digestnews.entity.Item;
+import com.iwillow.android.digestnews.http.RxNewsParser;
+import com.iwillow.android.lib.log.Log;
+import com.iwillow.android.lib.log.LogUtil;
+
+import java.util.List;
+
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 
 public class NewsListActivity extends AppCompatActivity implements ProductGuideDialog.OnFragmentInteractionListener {
+    private Subscriber<List<Item>> subscriber;
+    public static final String PREFS_NAME = "MyPrefsFile";
+    private static final String TAG = "NewsListActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_news_list);
         // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         // setSupportActionBar(toolbar);
@@ -27,7 +43,35 @@ public class NewsListActivity extends AppCompatActivity implements ProductGuideD
                 dialog.show(getSupportFragmentManager(), "product_tour");
             }
         });
+
+        subscriber = new Subscriber<List<Item>>() {
+            @Override
+            public void onCompleted() {
+                LogUtil.d(TAG, "onCompleted()");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                LogUtil.e(TAG, "onError:" + e.getMessage());
+            }
+
+            @Override
+            public void onNext(List<Item> items) {
+                if (items != null && items.size() > 0) {
+                    LogUtil.d(TAG, "onNext:" + items.get(0).getTitle());
+                } else {
+                    Log.e(TAG, "DATA IS NULL");
+                }
+            }
+        };
+        RxNewsParser.getNewsContent("74eabc4d-5889-35ca-80da-08ec47e55223")
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(subscriber);
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -55,4 +99,15 @@ public class NewsListActivity extends AppCompatActivity implements ProductGuideD
     public void onFragmentInteraction(Uri uri) {
 
     }
+
+    @Override
+    protected void onDestroy() {
+        if (subscriber != null) {
+            subscriber.unsubscribe();
+        }
+        super.onDestroy();
+
+    }
+
+
 }
